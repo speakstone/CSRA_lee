e75cfecf4c71e8c9040977b7923ef9a47ac71196
speakstone <1549640983@qq.com>
2022-08-03 23:21:37 +0800

    readme修改

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ def Args():
     parser.add_argument("--model", default="resnet101", type=str)
     parser.add_argument("--num_heads", default=1, type=int)
     parser.add_argument("--lam", default=0.1, type=float)
-    parser.add_argument("--load_from", default="checkpoint/resnet101/epoch_22.pth", type=str)
+    parser.add_argument("--load_from", default="checkpoint/resnet101/epoch_1.pth", type=str)
     # dataset
     parser.add_argument("--datadir", default="/work/dataset/huawei_2022_2/test_images/", type=str)
     parser.add_argument("--csv", default="/work/dataset/huawei_2022_2/submission.csv", type=str)
